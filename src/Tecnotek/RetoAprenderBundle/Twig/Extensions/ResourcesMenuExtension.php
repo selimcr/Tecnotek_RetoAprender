1e5e68d7402422663f802cdc114a7b04acfcffb6
Selim Diaz <sdiaz@cecropiasolutions.com>
2013-06-26 22:44:09 -0600

    Updating admin console

--- src/Tecnotek/RetoAprenderBundle/Twig/Extensions/ResourcesMenuExtension.php
+++ src/Tecnotek/RetoAprenderBundle/Twig/Extensions/ResourcesMenuExtension.php
@@ -16,7 +16,11 @@ class ResourcesMenuExtension extends \Twig_Extension
         return array(
             'topics' => new \Twig_Function_Method($this, 'getTopics'),
             'levels' => new \Twig_Function_Method($this, 'getLevels'),
+            'include_raw' => new \Twig_Function_Method($this, 'twig_include_raw'),
+            //'include_raw' => new \Twig_Function_Function('twig_include_raw', array('needs_environment' => true)),
         );
+
+        //$twig->addFunction('', new Twig_Function_Function());
     }
 
     public function getTopics()
@@ -31,10 +35,15 @@ class ResourcesMenuExtension extends \Twig_Extension
         return $this->conn->fetchAll($sql);
     }
 
+    public function twig_include_raw($file) {
+        return file_get_contents($file);
+    }
+
     public function getFilters()
     {
         return array(
             'price' => new \Twig_Filter_Method($this, 'priceFilter'),
+            'includePresentationFile' => new \Twig_Filter_Method($this, 'includePresentationFile'),
         );
     }
 
